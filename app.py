25fc5883132637d70dfdb730af914c65a4a22fa6
Matthew Jeng <mjeng@berkeley.edu>
2019-03-30 10:43:12 -0700

    assistant working, lacking api integration

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, render_template
 import os
+import assistant
 
 app = Flask(__name__)
 @app.route("/", methods=["GET"])
@@ -9,6 +10,12 @@ def homepage():
 @app.route("/heatmap", methods=["GET"])
 def heatmap():
     return render_template("heatmap.html")
+
+@app.route("/assistant", methods=["POST"])
+def startcall():
+    assistant.run()
+    return "done"
+
 
 
 if __name__ == "__main__":
