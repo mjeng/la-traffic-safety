659a3f37dfe0d55f9733388ef3b7343188ca067f
Matthew Jeng <mjeng@berkeley.edu>
2019-03-30 04:55:41 -0700

    flask structure

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, request, render_template
+import os
+
+app = Flask(__name__)
+@app.route("/", methods=["GET"])
+def homepage():
+    return render_template("index.html")
+
+@app.route("/heatmap", methods=["GET"])
+def heatmap():
+    return render_template("heatmap.html")
+
+
+if __name__ == "__main__":
+    app.run()
