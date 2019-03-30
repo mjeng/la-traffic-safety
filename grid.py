d82f0a12171f3571e6d5799091d7ecca9130322f
Ray Yuan <ray.x.yuan@gmail.com>
2019-03-30 06:35:43 -0700

    lol

--- grid.py
+++ grid.py
@@ -21,53 +21,53 @@ class Grid:
         self.grid = pick.grid
 
 
-    def create_grid(data_file, pickle_file_name = None):
+    def create_grid(self,data_file, pickle_file_name = None):
         """
         Takes in raw data, creates Grid instance.
         """
         #read CSV as pandas dataframe
         data = pd.read_csv(data_file)
 
-        y_num = (max_lat - min_lat + 1) // step_size
-        x_num = (max_lon - min_lon + 1) // step_size
+        y_num = (self.max_lat - self.min_lat + 1) // self.step_size
+        x_num = (self.max_lon - self.min_lon + 1) // self.step_size
         grid = [[[None] for i in range(x_num)] for j in range(y_num)]
         for index, row in data.iterrows():
-            Point p = Point(row['Latitude'], row['Longitude'])
-            row_index, col_index = index_helper(p)
+            p = Point(row['Latitude'], row['Longitude'])
+            row_index, col_index = self.index_helper(p)
             grid[row_index][col_index].append(p)
         self.grid = grid
         pickle.dump(self, open(pickle_file_name, "wb" ) )
 
-    def get_score(pt, radius):
-        actual_row_index, actual_col_index = index_helper(pt)
+    def get_score(self,pt, radius):
+        actual_row_index, actual_col_index = self.index_helper(pt)
         potential_pts = []
-        new_max_lat, col_index = index_helper(Point(pt.get_lat() + radius, pt.get_lon()))
-        new_min_lat, col_index = index_helper(Point(pt.get_lat() - radius, pt.get_lon()))
-        row_index, new_max_lon = index_helper(Point(pt.get_lat(), pt.get_lon() + radius))
-        row_index, new_min_lon = index_helper(Point(pt.get_lat(), pt.get_lon() - radius))
+        new_max_lat, col_index = self.index_helper(Point(pt.get_lat() + radius, pt.get_lon()))
+        new_min_lat, col_index = self.index_helper(Point(pt.get_lat() - radius, pt.get_lon()))
+        row_index, new_max_lon = self.index_helper(Point(pt.get_lat(), pt.get_lon() + radius))
+        row_index, new_min_lon = self.index_helper(Point(pt.get_lat(), pt.get_lon() - radius))
 
-        for (lat in range(new_min_lat, new_max_lat+1)):
-            for (lon in range(new_min_lon, new_max_lon+1)):
-                if (in_bounds(lat, lon)):
+        for lat in range(new_min_lat, new_max_lat+1):
+            for lon in range(new_min_lon, new_max_lon+1):
+                if self.in_bounds(lat, lon):
                     potential_pts.extend(self.grid[lat][lon])
 
         actual_num_valid = 0
-        for (p in potential_pts):
+        for p in potential_pts:
             squared_dist = (pt.get_lat() - p.get_lat())**2 + (pt.get_lon() - p.get_lon())**2
             if squared_dist ** 0.5 < radius:
-                actual_num_valid++
+                actual_num_valid+=1
         return actual_num_valid
 
-    def index_helper(pt):
-        row_index = (pt.get_lat() - self.min_lat) // step_size
-        col_index = (pt.get_lon() - self.min_lon) // step_size
+    def index_helper(self,pt):
+        row_index = (pt.get_lat() - self.min_lat) // self.step_size
+        col_index = (pt.get_lon() - self.min_lon) // self.step_size
         return row_index, col_index
 
-    def in_bounds(row_index, col_index):
-        y_num = (max_lat - min_lat + 1) // step_size
-        x_num = (max_lon - min_lon + 1) // step_size
-        if (row_index < 0 || row_index > (y_num - 1)):
+    def in_bounds(self,row_index, col_index):
+        y_num = (self.max_lat - self.min_lat + 1) // self.step_size
+        x_num = (self.max_lon - self.min_lon + 1) // self.step_size
+        if row_index < 0 or row_index > (y_num - 1):
             return False
-        if (col_index < 0 || col_index > (x_num - 1)):
+        if col_index < 0 or col_index > (x_num - 1):
             return False
         return True
---
+++ test_grid.py
@@ -0,0 +1,5 @@
+from point import Point
+from grid import Grid
+import all_scores
+
+g = Grid(33.707,34.3343,-118.1554,-118.6661,.01)
