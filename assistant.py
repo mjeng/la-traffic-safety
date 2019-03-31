17476cdba3fc9fbfaab8d008162911fd3a60d27b
Matthew Jeng <mjeng@berkeley.edu>
2019-03-31 04:41:36 -0700

    moved env to env_variables.yaml

--- assistant.py
+++ assistant.py
@@ -1,24 +1,24 @@
 from twilio.rest import Client
-import polling, time
+import polling, time, os
 
-CURR = None
-INTRO = "https://handler.twilio.com/twiml/EHab9c307ffe2116b32a69ff4fa9447522"
-ALLGOOD = "https://handler.twilio.com/twiml/EHa16958d8d15597313ec30061f6b2421d"
-WARN = "https://handler.twilio.com/twiml/EH4b40e44feb9cfe09d886d5212d27fb45"
-HANGUP = "https://handler.twilio.com/twiml/EHc91eed7f8e8ba9a05b802fbd6cee5cab"
+INTRO = os.environ.get("TWIML_INTRO")
+ALLGOOD = os.environ.get("TWIML_ALLGOOD")
+WARN = os.environ.get("TWIML_WARN")
+HANGUP = os.environ.get("TWIML_HANGUP")
 
-account_sid = 'AC91e95fc48cb37932aa55ca99d58f40d9'
-auth_token = '368d12d3f3e75f039a206d11e0387c64'
+account_sid = os.environ.get("TWILIO_SID")
+auth_token = os.environ.get("TWILIO_AUTH")
 client = Client(account_sid, auth_token)
 
 class Call:
     GOOD = "good"
     BAD = "bad"
     def __init__(self):
+        print(list(os.environ.items()))
         self.call = client.calls.create(
                                 url=INTRO,
-                                to='+19739085890',
-                                from_='+12018176781'
+                                to=os.environ.get("TWILIO_TO"),
+                                from_=os.environ.get("TWILIO_FROM")
                             )
         self.last_state = Call.GOOD
 
